a69e7db28d2e3b89e78905ffbfd3bdfe8c2045d6
sky <xiongqi19850926@163.com>
2018-01-25 16:28:13 +0800

    添加更多崩溃处理

---
+++ SolveCrashSDKTest/SolveCrashSDKTest/SubThreadViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubThreadViewController.h
+//  SolveCrashSDKTest
+//
+//  Created by sky on 2018/1/25.
+//  Copyright © 2018年 Appfactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubThreadViewController : UIViewController
+
+@end
